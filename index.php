566a48ef3b8187c0812bb028f5ec80d282990307
Amar Motghare <60289520+amarmotghare@users.noreply.github.com>
2021-01-30 13:49:58 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!<!DOCTYPE html>
+
+<html>
+
+  <head>
+
+<title>pdf sample</title>
+
+  </head>
+
+  <body>
+
+   <center>
+
+     <h2 style="color:#d7358e;">Welcome to the Marathi A1 Developers </h2>
+
+     <p><b>Click below to download PDF </b></p>
+
+     <a href="cleter.pdf" target="_Marathi A1">Download Pdf Now</a>
+
+     </center>
+
+     </body>
+
+</html>
